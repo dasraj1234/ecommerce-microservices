4a7c4e23d0d9c3303d4f7ef27b67c375020757d9
Ujjal Nath <ujjalnath008@gmail.com>
2026-04-28 11:23:23 +0530

    configured dev setup

--- auth-user-service/src/main/java/com/MCA/authN_Z/AuthNZApplication.java
+++ auth-user-service/src/main/java/com/MCA/authN_Z/AuthNZApplication.java
@@ -2,8 +2,15 @@ package com.MCA.authN_Z;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
 public class AuthNZApplication {
 
 	public static void main(String[] args) {
