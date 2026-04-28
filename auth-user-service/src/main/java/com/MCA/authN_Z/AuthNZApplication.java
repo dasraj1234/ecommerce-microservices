077eb131a2061be0f23593a6f1d2fdb936478801
Ujjal Nath <ujjalnath008@gmail.com>
2026-04-28 10:56:11 +0530

    auth starter

---
+++ auth-user-service/src/main/java/com/MCA/authN_Z/AuthNZApplication.java
@@ -0,0 +1,13 @@
+package com.MCA.authN_Z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthNZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthNZApplication.class, args);
+	}
+
+}
---
+++ auth-user-service/src/test/java/com/MCA/authN_Z/AuthNZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MCA.authN_Z;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthNZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
